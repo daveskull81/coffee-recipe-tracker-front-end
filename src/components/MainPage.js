c3c4f466ff50ec5832beee4dcddfc09fdac73265
dAVE Inden <davidinden@gmail.com>
2019-12-24 10:32:59 -0800

    add components for public content, login, and register forms

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Coffee Recipe Tracker</h1>
+      <p>Marketing type page about the application.</p>
+    </div>
+  );
+}
+
+export default App;
