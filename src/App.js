72331c04de8fe313685dcb816fd9d1cf65db3c95
dAVE Inden <davidinden@gmail.com>
2019-12-30 15:36:14 -0800

    test use of react-cookie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './components/Home';
+import { useCookies } from 'react-cookie';
 
 function App() {
+  const [ cookies, setCookie ] = useCookies(['testing']);
+
+  useEffect(() => {
+    setCookie('testing', 'this is a test', { path: '/', httpOnly: true, sameSite: true });
+  }, [setCookie])
+
   return (
     <div>
-      <Home />
+      <Home testProp={cookies.testing}/>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function App() {
+function App(props) {
   return (
     <div>
       <h1>Home</h1>
       <p>Cards linking to Coffess, Methods, and Recpies.</p>
+      <p>{props.testProp}</p>
     </div>
   );
 }
