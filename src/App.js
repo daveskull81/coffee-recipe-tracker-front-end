aca0cf3c9f85aeae9b75c89298ba8e58aa5543de
dAVE Inden <davidinden@gmail.com>
2020-01-02 11:22:06 -0800

    setup initial routes for user login/register and home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import MainPage from './components/MainPage';
 import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
 
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
+          <Route path='/'>
+            <MainPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
