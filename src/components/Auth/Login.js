a0cf584736196a75686732e91d5a5b5416ac14e9
dAVE Inden <davidinden@gmail.com>
2020-01-02 15:06:15 -0800

    implement login form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import apiRequest from '../../utils/apiRequest';
+import { useCookies } from 'react-cookie';
+import { useHistory } from 'react-router-dom';
 
 function Login() {
+
+  const history = useHistory();
+  const userCookieLabel = process.env.REACT_APP_USER_COOKIE_NAME;
+  const [ user, setUser ] = useState({ username: '', password: '' });
+  const [ cookies, setCookie ] = useCookies([userCookieLabel]);
+
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    apiRequest()
+      .post('/auth/login', user)
+      .then(res => {
+        console.log(res.data);
+        setCookie(userCookieLabel, res.data, { path: '/' });
+        history.push('/home');
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div>
-      <h1>Login Form</h1>
-    </div>
+    <form onSubmit={onSubmit}>
+      <div>
+        <TextField 
+          id='username'
+          label='Username'
+          name='username'
+          variant='outlined'
+          value={user.username}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <TextField
+          id='password'
+          label='Password'
+          type='password'
+          name='password'
+          variant='outlined'
+          value={user.password}
+          onChange={handleChange}
+        />
+      </div>
+      <Button variant='contained' color='primary' type='submit'>Submit</Button>
+    </form>
   );
 }
 
