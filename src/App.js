eb4e4a3eb2f8fe65ee011524d461b1d612f7a65b
dAVE Inden <davidinden@gmail.com>
2020-01-02 12:27:50 -0800

    set private routes for other app features

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Home from './components/Layout/Home';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import PrivateRoute from './components/Auth/PrivateRoute';
+import Coffees from './components/Coffees/Coffees';
+import Methods from './components/Methods/Methods';
+import Recipes from './components/Recipes/Recipes';
 
 function App() {
 
@@ -20,6 +23,9 @@ function App() {
             <Register />
           </Route>
           <PrivateRoute path='/home' component={Home} />
+          <PrivateRoute path='/coffees' component={Coffees} />
+          <PrivateRoute path='/methods' component={Methods} />
+          <PrivateRoute path='/recipes' component={Recipes} />
           <Route path='/'>
             <MainPage />
           </Route>
