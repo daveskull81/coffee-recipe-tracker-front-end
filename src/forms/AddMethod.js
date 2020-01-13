d4386683a749ea9a0f4f7b016ba15ce4dc7aec3d
dAVE Inden <davidinden@gmail.com>
2020-01-12 20:40:59 -0800

    Add Method form

---
+++ src/forms/AddMethod.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import apiRequest from '../utils/apiRequest';
+import { TextField, Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+
+const AddMethod = ({ currentUser }) => {
+
+    const [ method, setMethod ] = useState({
+        name: ''
+    });
+
+    const history = useHistory();
+
+    const onChangeHandler = e => {
+        setMethod({
+            ...method,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const onSubmitHandler = e => {
+        e.preventDefault();
+        apiRequest(currentUser.token)
+            .post(`/users/${currentUser.id}/methods`, { ...method, user_id: currentUser.id })
+            .then(() => {
+                setMethod({
+                    name: ''
+                });
+                history.push('/methods');
+            })
+            .catch(() => {
+                history.push('/login');
+            });
+    };
+
+    const cancelClickHandler = () => {
+        history.push('/methods');
+    };
+
+    return (
+        <>
+            <h2>Add Method</h2>
+            <form onSubmit={onSubmitHandler}>
+                <TextField 
+                    margin='normal'
+                    label='Name'
+                    name='name'
+                    onChange={onChangeHandler}
+                    value={method.name}
+                    fullWidth
+                >
+                </TextField>
+                <Button color='primary' type='submit'>Add Method</Button>
+                <Button color='secondary' onClick={cancelClickHandler}>Cancel</Button>
+            </form>
+        </>
+    );
+};
+
+export default AddMethod;
