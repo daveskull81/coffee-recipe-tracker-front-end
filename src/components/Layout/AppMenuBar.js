90f98896f058da9565b6437bccf1ec2271fb786b
dAVE Inden <davidinden@gmail.com>
2020-01-17 16:02:12 -0800

    add menu and menu items to app menu bar

--- src/components/Layout/AppMenuBar.js
+++ src/components/Layout/AppMenuBar.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from '@material-ui/core';
 import { Menu as MenuIcon } from '@material-ui/icons';
 import LogoutButton from './LogoutButton';
+import { Link } from 'react-router-dom';
 
 const AppMenuBar = ({ headingText }) => {
+
+    const [ anchorEl, setAnchorEl ] = useState(null);
+
+    const menuClickHandler = e => {
+        setAnchorEl(e.currentTarget);
+    };
+
+    const menuCloseHandler = e => {
+        setAnchorEl(null);
+    };
+
     return (
         <AppBar position='static'>
             <Toolbar>
-                <IconButton edge='start' color='inherit' aria-label='menu'>
+                <IconButton onClick={menuClickHandler} edge='start' color='inherit' aria-label='menu'>
                     <MenuIcon />
                 </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={menuCloseHandler}
+                >
+                    <MenuItem onClick={menuCloseHandler}><Link to='/home'>Home</Link></MenuItem>
+                    <MenuItem onClick={menuCloseHandler}><Link to='/coffees'>Coffees</Link></MenuItem>
+                    <MenuItem onClick={menuCloseHandler}><Link to='/methods'>Methods</Link></MenuItem>
+                    <MenuItem onClick={menuCloseHandler}><Link to='/recipes'>Recipes</Link></MenuItem>
+                </Menu>
                 <Typography variant='h5'>
                     {headingText}
                 </Typography>
