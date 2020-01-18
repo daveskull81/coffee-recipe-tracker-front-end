5f270cc509c25c095a98b2ce6ca3ab742d18b37c
dAVE Inden <davidinden@gmail.com>
2020-01-18 12:37:55 -0800

    set recipe cards to link to single recipe view page

--- src/views/AllRecipes.js
+++ src/views/AllRecipes.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import apiRequest from '../utils/apiRequest';
-import { Grid, Card, CardContent, CardActions, Button, Container } from '@material-ui/core'
+import { Grid, Card, CardContent, CardActionArea, Button, Container } from '@material-ui/core'
 import { Link, useHistory } from 'react-router-dom';
 
 const AllRecipes = ({ currentUser }) => {
@@ -18,18 +18,6 @@ const AllRecipes = ({ currentUser }) => {
         history.push('/login');
       });
   }, [setRecipes, currentUser.token, currentUser.id, history]);
-
-  const deleteHandler = recipeId => {
-    apiRequest(currentUser.token)
-      .delete(`/users/${currentUser.id}/recipes/${recipeId}`)
-      .then(() => {
-        const updatedRecipes = recipes.filter(recipe => recipe.id !== recipeId);
-        setRecipes(updatedRecipes);
-      })
-      .catch(() => {
-        history.push('/login');
-      });
-  };
 
   if (!recipes) {
     return (
@@ -56,23 +44,12 @@ const AllRecipes = ({ currentUser }) => {
             : recipes.map(recipe => {
               return <Grid item xs={12} key={recipe.id}>
                       <Card>
-                        <CardContent>
-                          <p><strong>Name:</strong> {recipe.name}</p>
-                          {/* Will use this code in single recipe view */}
-                          {/* <p><strong>Coffee Weight:</strong> {recipe.coffee_weight}</p>
-                          <p><strong>Total Weight:</strong> {recipe.total_weight}</p>
-                          <p><strong>Bloom:</strong> {recipe.bloom ? 'Yes' : 'No'}</p>
-                          <p><strong>Bloom Time:</strong> {recipe.bloom ? `${recipe.bloom_time} seconds` : 'n/a'}</p>
-                          <p><strong>Bloom Weight:</strong> {recipe.bloom ? `${recipe.bloom_weight}` : 'n/a'}</p>
-                          <p><strong>Total Time:</strong> {`${recipe.time_minutes} min(s) ${recipe.time_seconds} sec(s)`}</p>
-                          <p><strong>Grind Size:</strong> {recipe.grind_size}</p>
-                          <p><strong>Water Temperature:</strong> {recipe.water_temp}</p> */}
-                          <p><strong>Rating:</strong> {recipe.rating}</p>
-                        </CardContent>
-                        <CardActions>
-                          <Link to={`/recipes/edit/${recipe.id}`}><Button>Edit</Button></Link>
-                          <Button color="secondary" onClick={() => deleteHandler(recipe.id)}>Delete</Button>
-                        </CardActions>
+                        <CardActionArea component={Link} to={`/recipes/${recipe.id}`}>
+                          <CardContent>
+                            <p><strong>Name:</strong> {recipe.name}</p>
+                            <p><strong>Rating:</strong> {recipe.rating}</p>
+                          </CardContent>
+                        </CardActionArea>
                       </Card>
                     </Grid>
             })
